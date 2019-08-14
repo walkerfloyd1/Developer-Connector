8925a18dec30995b3fdc9f0df9cd385fe2707f73
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-14 12:45:09 -0700

    connectDB working

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const connectDB = require('./config/db.js');
+
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
