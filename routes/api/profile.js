89adb81073005069c8910efda89939e3de7e61ab
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-14 21:37:21 -0700

    experience not working

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -109,8 +109,6 @@ router.post('/', [ auth, [
     }
 
     console.log(profileFields.skills);
-
-    res.send("Hello")
 })
 
 // GET api/profile
@@ -164,5 +162,58 @@ router.delete('/', auth, async (req, res) => {
         res.status(500).send('Server Error')
     }
 })
+
+// PUT api/profile/experience
+// @desc Add profile experience
+// @access Private
+
+router.put('/experience', [auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From Date is required').not().isEmpty()
+]
+], 
+async (req, res) => {
+    const errors = validationResult(req);
+   
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        title, 
+        company, 
+        location, 
+        from, 
+        to, 
+        current, 
+        description
+    } = req.body;
+
+    const newExp = {
+        title, 
+        company, 
+        location, 
+        from, 
+        to, 
+        current, 
+        description
+    };
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        if (!profile) {return res.status(400).send('No profile found.');}
+
+        profile.experience.unshift(newExp);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+});
 
 module.exports = router;
