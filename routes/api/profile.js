b02ef966244db1297293f53123e21d52ca0ed1bb
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-14 19:56:51 -0700

    delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -148,4 +148,21 @@ router.get('/user/:user_id', async (req, res) => {
     }
 })
 
+// DELETE api/profile
+// @desc Delete users profile, user, posts
+// @access Private
+
+router.delete('/', auth, async (req, res) => {
+    try {
+        // todo- remove users posts
+        // remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        await User.findOneAndRemove({ _id: req.user.id });
+        res.json({ msg: "User deleted" });
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router;
