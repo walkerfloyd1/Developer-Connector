515f38ae82a79a4a519abb1d63da3c96b9039e4d
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-19 12:12:53 -0700

    create profile component

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 
 if(localStorage.token) {
   setAuthToken(localStorage.token);
@@ -33,6 +34,7 @@ return (
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
           <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <PrivateRoute exact path="/create-profile" component={CreateProfile} />
         </Switch>
       </section>
     </Fragment>
