2791ea8a3ffeae7cf654d837f783bfe809614d87
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-19 11:49:46 -0700

    displaying if logged in user has a profile on dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
     AUTH_ERROR,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
-    LOGOUT
+    LOGOUT,
+    CLEAR_PROFILE
 } from './types';
 import setAuthToken from '../utils/setAuthToken'
 
@@ -107,6 +108,10 @@ export const login = (
 
 export const logout = () => dispatch => {
     dispatch({
+        type: CLEAR_PROFILE
+    });
+    dispatch({
         type: LOGOUT
-    })
+    });
+
 }
---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import loading from './loading.gif';
+
+export default () => (
+    <Fragment>
+        <img 
+        src={loading}
+        style={{ width: '200px', margin: 'auto', display: 'block'}}
+        alt="Loading..." />
+    </Fragment>
+);
