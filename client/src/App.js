2f7c0388342a380348cad1344bedee7488b244f4
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-18 22:58:59 -0700

    loading user and checking auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Navbar from './components/layout/Navbar';
@@ -8,8 +8,18 @@ import Login from './components/auth/Login';
 import { Provider } from 'react-redux';
 import store from './store';
 import Alert from './components/layout/Alert';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken'
 
-const App = () => 
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+useEffect(() => {
+  store.dispatch(loadUser());
+}, []);
+return (
 <Provider store={store}>
 <Router>
     <Fragment >
@@ -24,7 +34,7 @@ const App = () =>
       </section>
     </Fragment>
     </Router>
-    </Provider>
-
+    </Provider>)
+}
 
 export default App;
