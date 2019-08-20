0d418e6ba6e81c1eaa9260fbb7c88b237b4c6e1c
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-20 14:50:31 -0700

    post component showing

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem.js';
 
 const Posts = ({ getPosts, post: {
     posts,
@@ -11,10 +12,20 @@ const Posts = ({ getPosts, post: {
     useEffect(() => {
         getPosts();
 }, [getPosts]);
-    return (
-        <div>
-            
-        </div>
+    return loading ? <Spinner /> : (
+        <Fragment>
+            <h1 className="large text-primary">
+                Posts
+            </h1>
+            <p className="lead">
+                <i className="fas fa-user" /> Welcome to the community
+            </p>
+            <div className="posts">
+                {posts.map(post => (
+                    <PostItem key={post._id} post={post} />
+                ))}
+            </div>
+        </Fragment>
     )
 }
 
