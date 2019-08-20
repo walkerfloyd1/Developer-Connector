2dae6eec41e74f6f67682e613f5d7e7e6de43b9f
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-20 11:00:52 -0700

    experience and education showing on profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,9 @@ import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
+
 
 const Profile = ({ getProfileById, match, auth, profile: { profile, loading } }) => {
     useEffect(() => {
@@ -24,6 +27,30 @@ const Profile = ({ getProfileById, match, auth, profile: { profile, loading } })
                 <div class="profile-grid my-1">
                     <ProfileTop profile={profile}/>
                     <ProfileAbout profile={profile}/>
+                    <div className="profile-exp bg-white p-2">
+                        <h2 className="text-primary">Experience</h2>
+                        {profile.experience.length > 0 ? (
+                            <Fragment>
+                                {profile.experience.map(experience => (
+                                    <ProfileExperience 
+                                    key={experience._id} 
+                                    experience={experience} />
+                                ))}
+                            </Fragment>
+                        ) : (<h4>No Experience Credentials</h4>)}
+                    </div>
+                    <div className="profile-edu bg-white p-2">
+                        <h2 className="text-primary">Education</h2>
+                        {profile.education.length > 0 ? (
+                            <Fragment>
+                                {profile.education.map(education => (
+                                    <ProfileEducation
+                                    key={education._id} 
+                                    education={education} />
+                                ))}
+                            </Fragment>
+                        ) : (<h4>No Education Credentials</h4>)}
+                    </div>
                 </div>
             </Fragment>}
         </Fragment>
