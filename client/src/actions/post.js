5844ce89c5183b1fa4863dd665267cb81c8698db
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-21 12:25:44 -0700

    adding comments functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,7 +6,9 @@ import {
     UPDATE_LIKES,
     DELETE_POST,
     ADD_POST,
-    GET_POST
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from './types';
 
 // getting posts
@@ -127,6 +129,54 @@ export const getPost = id => async dispatch => {
             type: GET_POST,
             payload: res.data
         });
+    } catch (error) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: error.response.statusText,
+            status: error.response.status }
+        })
+    }
+}
+
+//ADD COMMENT
+
+export const addComment = (postId, formData ) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    try {
+        const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
+
+        dispatch({
+            type: ADD_COMMENT,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Comment added', 'success'))
+    } catch (error) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: error.response.statusText,
+            status: error.response.status }
+        })
+    }
+}
+
+//DELETE COMMENT
+
+export const deleteComment = (postId, commentId ) => async dispatch => {
+    
+    try {
+        const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+        dispatch({
+            type: REMOVE_COMMENT,
+            payload: commentId
+        });
+
+        dispatch(setAlert('Comment removed', 'success'))
     } catch (error) {
         dispatch({
             type: POST_ERROR,
